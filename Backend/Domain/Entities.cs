f0e2b4d05ab03af88b7b92dfa8c4f96ffcf539f7
sandbxk <90904706+sandbxk@users.noreply.github.com>
2022-11-18 11:24:21 +0100

    Create initial entities

---
+++ Backend/Domain/Entities.cs
@@ -0,0 +1,40 @@
+﻿namespace Domain;
+
+public struct User
+{
+    public int Id { get; set; }
+    public string DisplayName { get; set;}
+    public string Email { get; set;}
+    
+    //TODO JWT
+}
+
+public struct List
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public IEnumerable<User> UserAccess { get; set; } // this is the list of users that have access to this list
+    public IEnumerable<ListItem> Items { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime Modified { get; set; }
+    public bool IsArchived { get; set; }
+
+}
+
+public struct UserAccess
+{
+    public User User { get; set; }
+    public bool CanEdit { get; set; }
+    public bool CanDelete { get; set; }
+}
+
+public struct ListItem
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int Quantity { get; set; }
+    public ListItemStatus Status { get; set; }
+    public ListItemCategory Category { get; set; }
+    
+    //TODO: Expected cost
+}
