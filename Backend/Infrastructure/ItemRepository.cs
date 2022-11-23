b0e9f7b53558979df910232ab7f7c738a82f7b23
PhilEZad <90150224+PhilEZad@users.noreply.github.com>
2022-11-23 14:41:37 +0100

    Changed context

--- Backend/Infrastructure/ItemRepository.cs
+++ Backend/Infrastructure/ItemRepository.cs
@@ -8,44 +8,33 @@ namespace Infrastructure;
 public class ItemRepository : IRepository<Item>
 {
     private DatabaseContext _dbContext;
-    private DbContextOptions<DatabaseContext> _opts;
 
-    public ItemRepository(DatabaseContext context, DbContextOptions<DatabaseContext> opts)
+    public ItemRepository(DatabaseContext context)
     {
         _dbContext = context;
-        _opts = opts;
     }
 
     public List<Item> All()
     {
-        using (var context = new DatabaseContext(_opts))
-        {
-            return context.ItemTable.Include(i => i.GroceryList).ToList();
-        }
+        return _dbContext.ItemTable.Include(i => i.GroceryList).ToList();
     }
 
     public Item Create(Item t)
     {
-        using (var context = new DatabaseContext(_opts))
-        {
-            t.GroceryList = context
+        t.GroceryList = _dbContext
                 .GroceryListsTable
                 .Find(t.GroceryListId) ?? throw new InvalidOperationException();
-            context.ItemTable.Add(t);
-            context.SaveChanges();
-            return t;
-        }
+        _dbContext.ItemTable.Add(t);
+        _dbContext.SaveChanges();
+        return t;
     }
 
     public Item Delete(int id)
     {
-        using (var context = new DatabaseContext(_opts))
-        {
-            var item = context.ItemTable.Find(id);
-            context.Remove(item);
-            context.SaveChanges();
-            return item;
-        }
+        var item = _dbContext.ItemTable.Find(id);
+        _dbContext.Remove(item);
+        _dbContext.SaveChanges();
+        return item;
     }
 
     public Item Single(long id)
