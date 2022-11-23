637eedccdae419c47d5d24ded3a599807df70746
PhilEZad <90150224+PhilEZad@users.noreply.github.com>
2022-11-23 01:34:57 +0100

    Seperated BuildDB into its own interface

--- Backend/Infrastructure/Interfaces/IRepository.cs
+++ Backend/Infrastructure/Interfaces/IRepository.cs
@@ -8,7 +8,9 @@ public interface IRepository<T>
     IEnumerable<T> SearchByName(string tName);
     T Single(long id);
     T Update(long id, T model);
-    
+}
+public interface IDatabase
+{
     void BuildDB();
 }
 
