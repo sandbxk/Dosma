731ad4260f0b57808188c25f06aa3274d61aeb40
Mads Mandahl-Barth <mads-barth@live.dk>
2022-11-24 18:40:28 +0100

    Working debug JWT generation

--- Backend/Application/Helpers/TokenGenerator.cs
+++ Backend/Application/Helpers/TokenGenerator.cs
@@ -1,11 +1,25 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using Domain;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Application.Helpers;
 
 public static class TokenGenerator
 {
-    internal static string GenerateToken(User user)
+    public static string GenerateToken(User user, byte[] serverSecret)
     {
-        throw new NotImplementedException();
+        List<Claim> claims = new()
+        {
+            new Claim("id", user.Id.ToString()),
+            new Claim("name", user.DisplayName),
+            new Claim("username", user.Username),
+        };
+
+        var payload = new JwtPayload(null, null, claims, DateTime.Now, DateTime.Now.AddMinutes(45));
+        var header = new JwtHeader(new SigningCredentials(new SymmetricSecurityKey(serverSecret), SecurityAlgorithms.HmacSha512));
+        var token = new JwtSecurityToken(header, payload);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
-} 
+}
--- Backend/Application/Interfaces/IAuthenticationService.cs
+++ Backend/Application/Interfaces/IAuthenticationService.cs
@@ -1,8 +1,8 @@
+using Application.DTOs;
+
 namespace Application.Interfaces;
 
 public interface IAuthenticationService
 {
-    public bool ValidateLogin(string username, string password, out string token);
-
-    public bool ValidateToken(string token, string[] claims);
+    public bool ValidateLogin(LoginRequestDTO login, out string result);
 }
