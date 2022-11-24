09922b2a4ba68c6c7cc287f65132256871ba53a3
Mads Mandahl-Barth <mads-barth@live.dk>
2022-11-24 14:23:07 +0100

    renaming

--- Backend/Application/DTOs/LoginRequestDTO.cs
+++ Backend/Application/DTOs/LoginRequestDTO.cs
@@ -1,6 +1,6 @@
 namespace Application.DTOs
 {
-    public class LoginDTO
+    public class LoginRequestDTO
     {
         public string Username { get; set; } = null!;
         public string Password { get; set; } = null!;
