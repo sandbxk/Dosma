4cd07642ca02bb1f2d54ae81cddcc6e36f2a69ca
Mads Mandahl-Barth <mads-barth@live.dk>
2022-11-24 13:23:49 +0100

    added empty auth service

--- Backend/Application/DTOs/TokenDTO.cs
+++ Backend/Application/DTOs/TokenDTO.cs
@@ -2,7 +2,7 @@ namespace Application.DTOs
 {
     public class TokenDTO
     {
-        public uint Status { get; set; } = 500;
+        public uint Status { get; set; } = 200;
         public string? ErrorMessage { get; set; }
         public string Token { get; set; } = null!;
     }
