46190dc6992bc6da09bf21544f789417018c233e
Mads Mandahl-Barth <mads-barth@live.dk>
2022-11-24 14:23:07 +0100

    renaming

--- Backend/API/Controllers/AuthController.cs
+++ Backend/API/Controllers/AuthController.cs
@@ -1,31 +1,31 @@
+
 using Application.DTOs;
 using Application.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Backend.API.Controllers
+namespace Backend.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class AuthController : ControllerBase
+    private readonly IAuthenticationService _authenticationService;
+
+    public AuthController(IAuthenticationService authenticationService)
     {
-        private readonly IAuthenticationService _authenticationService;
+        _authenticationService = authenticationService;
+    }
 
-        public AuthController(IAuthenticationService authenticationService)
+    [AllowAnonymous]
+    [HttpPost]
+    public IActionResult Login([FromBody] LoginRequestDTO loginDTO)
+    {
+        if (_authenticationService.ValidateLogin(loginDTO.Username, loginDTO.Password, out string token))
         {
-            _authenticationService = authenticationService;
+            return Ok(new TokenResponseDTO { Token = token });
         }
 
-        [AllowAnonymous]
-        [HttpPost]
-        public IActionResult Login([FromBody] LoginDTO loginDTO)
-        {
-            if (_authenticationService.ValidateLogin(loginDTO.Username, loginDTO.Password, out string token))
-            {
-                return Ok(new TokenDTO { Token = token });
-            }
-
-            return BadRequest(new TokenDTO { Status = 400, ErrorMessage = "Invalid username or password" });
-        }
+        return BadRequest(new TokenResponseDTO { Status = 400, ErrorMessage = "Invalid username or password" });
     }
 }
