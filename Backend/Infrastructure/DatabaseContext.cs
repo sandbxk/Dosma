e6807266e257325fb3a4bc6ebc4026bf450d9b18
Mads Mandahl-Barth <mads-barth@live.dk>
2022-11-24 14:23:42 +0100

    DB creation separation

--- Backend/Infrastructure/DatabaseContext.cs
+++ Backend/Infrastructure/DatabaseContext.cs
@@ -6,43 +6,18 @@ namespace Infrastructure;
 
 public class DatabaseContext : DbContext
 {
-    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
-    {
-    }
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) {}
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        //Setting Primary Keys
-        modelBuilder.Entity<User>()
-            .HasKey(u => u.Id)
-            .HasName("PK_User");
+        CreateUserTable(ref modelBuilder);
+        CreateItemTable(ref modelBuilder);
+        CreateGroceryListTable(ref modelBuilder);
 
-        modelBuilder.Entity<GroceryList>()
-            .HasKey(g => g.Id)
-            .HasName("PK_GroceryList");
-        
-        modelBuilder.Entity<Item>()
-            .HasKey(i => i.Id)
-            .HasName("PK_Item");
-        
-        //Auto ID generation
-        modelBuilder.Entity<User>()
-            .Property(u => u.Id)
-            .ValueGeneratedOnAdd();
-
-        modelBuilder.Entity<GroceryList>()
-            .Property(l => l.Id)
-            .ValueGeneratedOnAdd();
-
-        modelBuilder.Entity<Item>()
-            .Property(i => i.Id)
-            .ValueGeneratedOnAdd();
-        
         //One-To-Many Relationship
-
-
         //Many-To-Many Relationship
-            //GroceryList-Users Relationship
+        
+        //GroceryList-Users Relationship
         modelBuilder.Entity<UserList>()
             .HasKey(ul => new { ul.UserID, ul.GroceryListID });
         modelBuilder.Entity<UserList>()
@@ -53,6 +28,45 @@ public class DatabaseContext : DbContext
             .HasOne(ul => ul.User)
             .WithMany(ul => ul.GroceryLists)
             .HasForeignKey(ul => ul.UserID);
+    }
+
+    private void CreateGroceryListTable(ref ModelBuilder modelBuilder)
+    {
+        //Setting Primary Keys
+        modelBuilder.Entity<GroceryList>()
+            .HasKey(g => g.Id)
+            .HasName("PK_GroceryList");
+        
+        //Auto ID generation
+        modelBuilder.Entity<GroceryList>()
+            .Property(l => l.Id)
+            .ValueGeneratedOnAdd();
+    }
+
+    private void CreateItemTable(ref ModelBuilder modelBuilder)
+    {
+        //Setting Primary Keys
+        modelBuilder.Entity<Item>()
+            .HasKey(i => i.Id)
+            .HasName("PK_Item");
+
+        //Auto ID generation
+        modelBuilder.Entity<Item>()
+            .Property(i => i.Id)
+            .ValueGeneratedOnAdd();
+    }
+
+    private void CreateUserTable(ref ModelBuilder modelBuilder)
+    {
+        //Setting Primary Keys
+        modelBuilder.Entity<User>()
+            .HasKey(u => u.Id)
+            .HasName("PK_User");
+
+        //Auto ID generation
+        modelBuilder.Entity<User>()
+            .Property(u => u.Id)
+            .ValueGeneratedOnAdd();
     }
     
     public DbSet<Item> ItemTable { get; set; } = null!;
