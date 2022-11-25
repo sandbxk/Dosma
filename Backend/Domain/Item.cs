f67f42bd09399e556a4ba8771af40f280e4c1129
Rasmus Sandbæk <90904706+sandbxk@users.noreply.github.com>
2022-11-25 13:54:40 +0100

    Update Item.cs

--- Backend/Domain/Item.cs
+++ Backend/Domain/Item.cs
@@ -2,7 +2,7 @@
 
 public class Item
 {
-    public int? Id { get; set; }
+    public int Id { get; set; }
     public string Title { get; set; }
     public int Quantity { get; set; }
     //public ListItemStatus Status { get; set; }
