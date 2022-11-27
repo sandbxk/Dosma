5a2c60f4f04068a6a139f62d0811257e41bcd3b0
Mads Mandahl-Barth <mads-barth@live.dk>
2022-11-27 20:19:34 +0100

    Update GroceryList.cs

--- Backend/Domain/GroceryList.cs
+++ Backend/Domain/GroceryList.cs
@@ -3,9 +3,9 @@
 public class GroceryList
 {
     public int Id { get; set; } 
-    public string Title { get; set; } = string.Empty;
+    public string Title { get; set; }
     
-    public List<Item> Items { get; set; } = new List<Item>();
+    public List<Item> Items { get; set; }
     
-    public ICollection<UserList> Users { get; set; } = new List<UserList>();
+    public ICollection<UserList> Users { get; set; }
 }
