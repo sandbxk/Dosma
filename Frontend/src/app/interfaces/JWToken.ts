7d8a99f8eada7ba6780cc63abf31b81188603aaf
Mads Mandahl-Barth <mads-barth@live.dk>
2022-12-05 14:35:12 +0100

    serializer error fixed

--- Frontend/src/app/interfaces/JWToken.ts
+++ Frontend/src/app/interfaces/JWToken.ts
@@ -1,8 +1,8 @@
 
 export class JWToken {
-    userId: number = 0;
+    id: number = 0;
     name: string = '';
     username: string = '';
-    access_claims: string = "";
+    //access_claims: string = "";
     exp: string = '';
 }
--- Frontend/src/app/util/ObjectGenerator.ts
+++ Frontend/src/app/util/ObjectGenerator.ts
@@ -1,6 +1,7 @@
 import { User } from "../interfaces/User";
 import { Buffer} from 'buffer';
 import { JWToken } from "../interfaces/JWToken";
+import { ConditionalExpr } from "@angular/compiler";
 export class ObjectGenerator {
 
   public static userFromToken(token: string): User {
@@ -10,11 +11,11 @@ export class ObjectGenerator {
     let base64 = token.split('.');
     let payload = JSON.parse(Buffer.from(base64[1], 'base64').toString()) as JWToken;
 
-    user.id = payload.userId;
+    user.id = payload.id;
     user.username = payload.username;
     user.name = payload.name;
     user.expire_at = payload.exp;
-    user.access_claims = JSON.parse(payload.access_claims);
+    //user.access_claims = JSON.parse(payload.access_claims);
 
     return user;
   }
