f259b206bc7971fd38e1edfea856c6982eac2131
PhilEZad <90150224+PhilEZad@users.noreply.github.com>
2022-12-07 07:22:19 +0100

    Changed update method to patch

--- Backend/Infrastructure/GroceryListRepository.cs
+++ Backend/Infrastructure/GroceryListRepository.cs
@@ -1,11 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 using Domain;
 using Infrastructure.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure;
 
-public class GroceryListRepository : IRepository<GroceryList>
+public class GroceryListRepository : IGroceryListRepository
 {
     private DatabaseContext _dbContext;
     
@@ -59,8 +60,40 @@ public class GroceryListRepository : IRepository<GroceryList>
 
     public GroceryList Update(GroceryList model)
     {
-        _dbContext.GroceryListsTable.Update(model);
-        _dbContext.SaveChanges();
+        var existingList = _dbContext.GroceryListsTable.Find(model.Id) ?? throw new NullReferenceException("List not found.");
+
+        foreach (PropertyInfo prop in existingList.GetType().GetProperties())
+        {
+            var propertyName = prop.Name;
+            var value = prop.GetValue(model);
+            
+            if (value != null)
+            {
+                var field = existingList.GetType().GetProperty(propertyName);
+                if (field != null)
+                {
+                    field.SetValue(existingList, value);
+                }
+            }
+        }
+        
+        _dbContext.Update(existingList);
+        int change = _dbContext.SaveChanges();
+
+        if (change == 0)
+        {
+            throw new NullReferenceException("Unable to update list.");
+        }
         return model;
+    }
+
+    public List<UserList> AddUser(User user, GroceryList groceryList)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<UserList> RemoveUser(User user, GroceryList groceryList)
+    {
+        throw new NotImplementedException();
     }
 }
---
+++ Backend/Infrastructure/Interfaces/IGroceryListRepistory.cs
@@ -0,0 +1,9 @@
+﻿using Domain;
+
+namespace Infrastructure.Interfaces;
+
+public interface IGroceryListRepository : IRepository<GroceryList>
+{
+    public List<UserList> AddUser(User user, GroceryList groceryList);
+    public List<UserList> RemoveUser(User user, GroceryList groceryList);
+}
