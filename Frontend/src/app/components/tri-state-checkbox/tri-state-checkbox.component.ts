aeaa7091c4cf48ac0468375beed3b21d5aaf16de
sandbxk <90904706+sandbxk@users.noreply.github.com>
2022-12-07 14:56:33 +0100

    diy checkbox

--- Frontend/src/app/components/tri-state-checkbox/tri-state-checkbox.component.ts
+++ Frontend/src/app/components/tri-state-checkbox/tri-state-checkbox.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Status} from "../../interfaces/StatusEnum";
+import {animate, keyframes, state, style, transition, trigger} from "@angular/animations";
 
 
 /**
@@ -8,7 +9,35 @@ import {Status} from "../../interfaces/StatusEnum";
 @Component({
   selector: 'app-tri-state-checkbox',
   templateUrl: './tri-state-checkbox.component.html',
-  styleUrls: ['tri-state-checkbox.component.scss']
+  styleUrls: ['tri-state-checkbox.component.scss'],
+  animations: [trigger("inOutAnimation", [
+    state("in", style({ opacity: 1 })),
+    transition(":enter", [
+      animate(
+        100,
+        keyframes([
+          style({ opacity: 0, offset: 0 }),
+          style({ opacity: 0.25, offset: 0.25 }),
+          style({ opacity: 0.5, offset: 0.5 }),
+          style({ opacity: 0.75, offset: 0.75 }),
+          style({ opacity: 1, offset: 1 }),
+        ])
+      )
+    ]),
+    transition(":leave", [
+      animate(
+        100,
+        keyframes([
+          style({ opacity: 1, offset: 0 }),
+          style({ opacity: 0.75, offset: 0.25 }),
+          style({ opacity: 0.5, offset: 0.5 }),
+          style({ opacity: 0.25, offset: 0.75 }),
+          style({ opacity: 0, offset: 1 }),
+        ])
+      )
+    ])
+  ])
+]
 
 })
 export class TriStateCheckboxComponent {
@@ -24,13 +53,13 @@ export class TriStateCheckboxComponent {
    * The state of the checkbox.
    */
   @Input() status: Status = Status.Unchecked;
+  allStatus = Status;
 
   /**
    * Handles the change event and updates the state of the checkbox.
    * @param newState The new state of the checkbox.
    */
   onChange() {
-
     switch (this.status) {
       case Status.Unchecked:
         this.status = Status.Done;
@@ -42,11 +71,10 @@ export class TriStateCheckboxComponent {
         this.status = Status.Unchecked;
         break;
     }
-    console.log(this.status.toString());
     this.statusChangedEvent.emit(this.status);
   }
 
-  public get statusEnum() {
+  public get statusEnum(): typeof Status {
     return Status;
   }
 
--- Frontend/src/app/interfaces/StatusEnum.ts
+++ Frontend/src/app/interfaces/StatusEnum.ts
@@ -2,7 +2,7 @@
  * An enum that represents the different states of the checkbox.
  */
 export enum Status {
-  Done, // The checkbox is checked.
-  Skipped, // The checkbox is unchecked and displays a red 'X'.
-  Unchecked, // The checkbox is unchecked and displays an empty box.
+  Unchecked = 0, // The checkbox is unchecked and displays an empty box.
+  Done = 1, // The checkbox is checked.
+  Skipped = 2, // The checkbox is unchecked and displays a red 'X'.
 }
