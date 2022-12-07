98e65aea188047acaa447f86d405921f88d06a1e
Mads Mandahl-Barth <mads-barth@live.dk>
2022-12-07 19:00:25 +0100

    added authentication to the other controllers

--- Backend/API/Controllers/GroceryListController.cs
+++ Backend/API/Controllers/GroceryListController.cs
@@ -3,6 +3,7 @@ using Application.DTOs;
 using Application.Interfaces;
 using Domain;
 using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -12,10 +13,12 @@ namespace API.Controllers;
 public class GroceryListController : ControllerBase
 {
     private readonly IGroceryListService _groceryListService;
-    
-    public GroceryListController(IGroceryListService groceryListService)
+    private readonly IAuthenticationService _authenticationService;
+
+    public GroceryListController(IGroceryListService groceryListService, IAuthenticationService authenticationService)
     {
         _groceryListService = groceryListService;
+        _authenticationService = authenticationService;
     }
 
     
@@ -23,77 +26,118 @@ public class GroceryListController : ControllerBase
      * Get Lists by various methods
      */
     [HttpGet]
-    public IEnumerable<GroceryList> GetAllLists()
+    public ActionResult<IEnumerable<GroceryList>> GetAllLists([FromHeader] string token)
     {
-        return _groceryListService.GetAllLists();
+        if (_authenticationService.AuthenticateToken(token))
+        {
+            // TODO: Check authorization
+                // Forbid();
+
+            return Ok(_groceryListService.GetAllLists());
+        }
+
+        return Unauthorized("Could not be authenticated");
     }
 
     [HttpGet("{id}")]
-    public IEnumerable<GroceryList> GetListsByUser([FromBody] User user)
+    public ActionResult<IEnumerable<GroceryList>> GetListsByUser([FromBody] User user, [FromHeader] string token)
     {
-        return _groceryListService.GetListsByUser(user);
+        if (_authenticationService.AuthenticateToken(token))
+        {
+            // TODO: Check authorization
+                // Forbid();
+
+            return Ok(_groceryListService.GetListsByUser(user));
+        }
+
+        return Unauthorized("Could not be authenticated");
     }
 
     [HttpPost]
-    public ActionResult<GroceryList> CreateGroceryList(GroceryListDTO dto)
+    public ActionResult<GroceryList> CreateGroceryList(GroceryListDTO dto, [FromHeader] string token)
     {
-        try
+        if (_authenticationService.AuthenticateToken(token))
         {
-            var result = _groceryListService.Create(dto);
-            return Created("product/" + result.Id, result);
+            try
+            {
+                // TODO: Check authorization
+                    // Forbid();
+
+                var result = _groceryListService.Create(dto);
+                return Created("product/" + result.Id, result);
+            }
+            catch (ValidationException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
         }
-        catch (ValidationException e)
-        {
-            return BadRequest(e.Message);
-        }
-        catch (Exception e)
-        {
-            return StatusCode(500, e.Message);
-        }
+
+        return Unauthorized("Could not be authenticated");
     }
 
     [HttpPatch]
     [Route("{id}")]
-    public ActionResult<GroceryList> UpdateList([FromRoute] int id, [FromBody] GroceryList groceryList)
+    public ActionResult<GroceryList> UpdateList([FromRoute] int id, [FromBody] GroceryList groceryList, [FromHeader] string token)
     {
-        if (id != groceryList.Id)
+        if (_authenticationService.AuthenticateToken(token))
         {
-            throw new ValidationException("List ID does not match ID in URL.");
+            try
+            {
+                if (id != groceryList.Id)
+                {
+                    throw new ValidationException("List ID does not match ID in URL.");
+                }
+
+                // TODO: Check authorization
+                    // Forbid();
+
+                return Ok(_groceryListService.UpdateList(id, groceryList));
+            }
+            catch (ValidationException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
         }
-        
-        try
-        {
-            return Ok(_groceryListService.UpdateList(id, groceryList));
-        }
-        catch (ValidationException e)
-        {
-            return BadRequest(e.Message);
-        }
-        catch (Exception e)
-        {
-            return StatusCode(500, e.Message);
-        }
+
+        return Unauthorized("Could not be authenticated");
     }
     
     [HttpDelete]
     [Route("{id}")]
-    public ActionResult DeleteList([FromRoute] int id, [FromBody] GroceryList groceryList)
+    public ActionResult DeleteList([FromRoute] int id, [FromBody] GroceryList groceryList, [FromHeader] string token)
     {
-        if (id != groceryList.Id)
+        if (_authenticationService.AuthenticateToken(token))
         {
-            throw new ValidationException("List ID does not match ID in URL.");
+            try
+            {
+                if (id != groceryList.Id)
+                {
+                    throw new ValidationException("List ID does not match ID in URL.");
+                }
+                
+                // TODO: Check authorization
+                    // Forbid();
+                    
+                return Ok(_groceryListService.DeleteList(groceryList));
+            }
+            catch (ValidationException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
         }
-        try
-        {
-            return Ok(_groceryListService.DeleteList(groceryList));
-        }
-        catch (ValidationException e)
-        {
-            return BadRequest(e.Message);
-        }
-        catch (Exception e)
-        {
-            return StatusCode(500, e.Message);
-        }
+
+        return Unauthorized("Could not be authenticated");
     }
 }
--- Backend/API/Controllers/ItemController.cs
+++ Backend/API/Controllers/ItemController.cs
@@ -11,80 +11,110 @@ namespace API.Controllers;
 public class ItemController : ControllerBase
 {
     private readonly IItemService _itemService;
+    private readonly IAuthenticationService _authenticationService;
 
-    public ItemController(IItemService itemService)
+    public ItemController(
+        IItemService itemService, 
+        IAuthenticationService authenticationService
+        )
     {
         _itemService = itemService;
+        _authenticationService = authenticationService;
     }
 
     [HttpPost]
-    public ActionResult<Item> CreateItem([FromBody] ItemDTO item)
+    public ActionResult<Item> CreateItem([FromBody] ItemDTO item, [FromHeader] string token)
     {
-        try
+        if (_authenticationService.AuthenticateToken(token))
         {
-            var result = _itemService.AddItem(item);
-            return Created("Item/" + result.Id, result);
+            try
+            {
+                // TODO: Check authorization
+                    // Forbid();
+
+                var result = _itemService.AddItem(item);
+                return Created("Item/" + result.Id, result);
+            }
+            catch (ValidationException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
         }
-        catch (ValidationException e)
-        {
-            return BadRequest(e.Message);
-        }
-        catch (Exception e)
-        {
-            return StatusCode(500, e.Message);
-        }
+
+        return Unauthorized("Could not be authenticated");
     }
 
     [HttpPatch]
     [Route("{id}")]
-    public ActionResult<Item> UpdateItem([FromRoute] int id, [FromBody] Item item)
+    public ActionResult<Item> UpdateItem([FromRoute] int id, [FromBody] Item item, [FromHeader] string token)
     {
-        if (id != item.Id)
-            throw new ValidationException("Item ID does not match ID in URL.");
-        
-        try
+        if (_authenticationService.AuthenticateToken(token))
         {
-            var result = _itemService.UpdateItem(item);
+            if (id != item.Id)
+                throw new ValidationException("Item ID does not match ID in URL.");
             
-            if (result == null)
+            try
             {
-                return NotFound();
+                // TODO: Check authorization
+                    // Forbid();
+
+                var result = _itemService.UpdateItem(item);
+                
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                return Ok("Item has been updated.");
             }
-            return Ok("Item has been updated.");
+            catch (ValidationException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
         }
-        catch (ValidationException e)
-        {
-            return BadRequest(e.Message);
-        }
-        catch (Exception e)
-        {
-            return StatusCode(500, e.Message);
-        }
+
+        return Unauthorized("Could not be authenticated");
     }
     
     
     [HttpDelete]
     [Route("{id}")]
-    public ActionResult DeleteItem([FromRoute] int id, [FromBody] Item item)
+    public ActionResult DeleteItem([FromRoute] int id, [FromBody] Item item, [FromHeader] string token)
     {
-        if (id != item.Id)
-            throw new ValidationException("Item ID does not match ID in URL.");
-        try
+        if (_authenticationService.AuthenticateToken(token))
         {
-            var result = _itemService.DeleteItem(item);
-            
-            if (result)
-                return Ok(item.Title + " has been deleted.");
-            else
-                return StatusCode(304, "Item could not be deleted.");
+            if (id != item.Id)
+                throw new ValidationException("Item ID does not match ID in URL.");
+
+            try
+            {
+                // TODO: Check authorization
+                    // Forbid();
+
+                var result = _itemService.DeleteItem(item);
+
+                if (result)
+                    return Ok(item.Title + " has been deleted.");
+                else
+                    return StatusCode(304, "Item could not be deleted.");
+            }
+            catch (ValidationException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
         }
-        catch (ValidationException e)
-        {
-            return BadRequest(e.Message);
-        }
-        catch (Exception e)
-        {
-            return StatusCode(500, e.Message);
-        }
+
+        return Unauthorized("Could not be authenticated");
     }
 }
